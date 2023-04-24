ccd2aaffe7a979c9d086a17f01ad48091b049f6f
valluvan23 <thiruvalluvan2303@gmail.com>
2023-04-24 21:22:59 +0530

    added a templates for recipes card

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Recipes } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
-export class RecipesListComponent {
+export class RecipesListComponent{
+  recipes: Recipes[]=[ new Recipes('Test Rceipes', 'This is a sample test','https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg')];
+  constructor(){}
+  
 
 }
