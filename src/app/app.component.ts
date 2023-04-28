d5d581570dcdbd8584c9bbc18b375244edcdd074
valluvan23 <thiruvalluvan2303@gmail.com>
2023-04-28 19:34:54 +0530

    showing either recipe component or shopping component at a time using data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Food_Project';
+  feature:string='recipe';
+
+  onselectfeature(feacturecom:string){
+    this.feature= feacturecom;
+
+  }
 }
